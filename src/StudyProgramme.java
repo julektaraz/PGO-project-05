8d220e13cfc42016520ddbd98d13f20507600936
julektaraz <s31958@pjwstk.edu.pl>
2024-06-09 21:28:03 +0200

    adding new classes

---
+++ src/StudyProgramme.java
@@ -0,0 +1,23 @@
+
+public class StudyProgramme {
+    private String name;
+    private String description;
+    private int numberOfSemesters;
+    private int allowedITN;
+
+    public StudyProgramme(String name, String description, int numberOfSemesters, int allowedITN) {
+        this.name = name;
+        this.description = description;
+        this.numberOfSemesters = numberOfSemesters;
+        this.allowedITN = allowedITN;
+    }
+
+    public int getAllowedITN() {
+        return this.allowedITN;
+    }
+
+    public int getNumberOfSemesters() {
+        return this.numberOfSemesters;
+    }
+
+}
